a5ff9fcc5425512cde7ca9d95eadb0564a9c3fad
Hack Reactor Student <student@hackreactor.com>
2017-06-02 19:09:11 -0700

    passes all tests up to and including App

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,30 +1,15 @@
-class VideoListEntry extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.handleVideoSelectedChange = this.handleVideoSelectedChange.bind(this);
-  }
-
-  handleVideoSelectedChange(e) {
-    console.log("This is running");
-    this.props.onVideoSelected(this.props.video);
-  }
-
-  render() {
-    return (
+var VideoListEntry = (props) =>(
       <div className="video-list-entry">
         <div className="media-left media-middle">
-          <img className="media-object" src={this.props.video.snippet.thumbnails.default.url} alt="" />
+          <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
         </div>
         <div className="media-body">
           <div className="video-list-entry-title"
-               onClick={this.handleVideoSelectedChange}
-          >{this.props.video.snippet.title}</div>
-          <div className="video-list-entry-detail">{this.props.video.snippet.description}</div>
+               onClick={()=> { props.onVideoSelected(props.video); } }
+          >{props.video.snippet.title}</div>
+          <div className="video-list-entry-detail">{props.video.snippet.description}</div>
         </div>
       </div> );
-  } 
-}
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
