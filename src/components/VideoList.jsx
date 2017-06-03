32919fa16ba3be90d3eb6876793867a27a88ea53
Hack Reactor Student <student@hackreactor.com>
2017-06-02 18:52:43 -0700

    videos are loaded when clicked

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,9 +1,12 @@
 var VideoList = (props) => (
-  <div className="video-list media">
-    {props.videos.map(video =>
-      <VideoListEntry video={video} />
-    )}
-  </div>
+    <div className="video-list media">
+      {props.videos.map(video =>
+        <VideoListEntry 
+          video={video}
+          onVideoSelected={props.onVideoSelected}
+        />
+      )}
+    </div> 
 );
 
 // PropTypes tell other developers what `props` a component expects
@@ -15,3 +18,4 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
+
